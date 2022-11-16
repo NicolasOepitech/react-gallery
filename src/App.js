e6ee5838a35d3238a070ee663aea3a3cd8c298dc
NicolasOepitech <113346605+NicolasOepitech@users.noreply.github.com>
2022-11-16 15:37:10 +0100

    Create

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import Photos from './Components/Photos/Photos'
+import {Routes, Route} from 'react-router-dom'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div>
+      <Navbar />
 
-export default App;
+      <Routes>
+        <Route path="/" element={<Photos />} />
+        <Route
+      </Routes>
+
+      
+    </div>
+  )
+}
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+        <h1>Navbar</h1>
+
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+
+    </div>
+  )
+}
